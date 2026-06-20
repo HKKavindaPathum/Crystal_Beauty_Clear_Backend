30d74a73852d2ee6013e8b7d10f496e480a8882f
HKKavindaPathum <kavindapathum98@gmail.com>
2026-06-20 18:45:20 +0530

    fix: use dynamic port for railway deployment

--- index.js
+++ index.js
@@ -57,8 +57,10 @@ app.use("/api/users" , userRouter)
 app.use("/api/orders", orderRouter)
 app.use("/api/reviews", reviewRouter)
 
-app.listen(3000,
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,
     ()=>{
-        console.log('Server is running on port 3000');
+        console.log(`Server is running on port ${PORT}`);
     }
 )
